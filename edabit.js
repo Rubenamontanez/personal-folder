4a95b66a6be30e03c62ddde41673c3bd5133ef84
Ruben Montanez <ruben.a.montanez@gmail.com>
2019-11-30 15:18:59 -0600

    additions to code

--- edabit.js
+++ edabit.js
@@ -24,3 +24,32 @@ function stackBoxes(n) {
 function isEvenOrOdd(num) {
     return num % 2 ? 'odd' : 'even';
 }
+
+function toInt(str) {
+    return Number(str)
+}
+
+function toStr(int) {
+    return String(int)
+}
+
+// returns the number of numbers in a string
+function findDigitAmount(num) {
+    return num.toString().length;
+}
+
+// returns true if number is within the range of min and max
+function isInRange(num, range) {
+    return num >= range.min && num <= range.max;
+}
+
+//returns the negated solution
+function negate(arr) {
+    return arr.map(val = > -val;
+)
+}
+
+function negate(arr) {
+    return arr.map(val => -val;
+)
+}
