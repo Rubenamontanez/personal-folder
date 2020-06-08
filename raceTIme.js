3135aceee729194d566f3cee4f3f99bf705009b8
Ruben Montanez <ruben.a.montanez@gmail.com>
2020-06-08 18:10:54 -0500

    more tomorrow

---
+++ raceTIme.js
@@ -0,0 +1,15 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+
+let registeredEarly  =  true;
+
+let runnerAge = 19;
+
+if (registeredEarly && runnerAge > 18){
+    raceNumber += 1000;
+}
+
+if (registeredEarly && runnerAge > 18){
+    console.log('race time is at 9:30 am')
+} else if (runner > 18 && !registerEarly){
+    console.log('Late adults run at 11:00 am, your race number ');
+}
