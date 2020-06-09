43330eeebae9fb88da4d14d6d3b22fe9dd044e36
Ruben Montanez <ruben.a.montanez@gmail.com>
2020-06-09 09:37:26 -0500

    still at it

--- raceTIme.js
+++ raceTIme.js
@@ -9,7 +9,12 @@ if (registeredEarly && runnerAge > 18){
 }
 
 if (registeredEarly && runnerAge > 18){
-    console.log('race time is at 9:30 am')
-} else if (runner > 18 && !registerEarly){
-    console.log('Late adults run at 11:00 am, your race number ');
+    console.log(`Race time is at 9:30 am and your race number is ${raceNumber}`)
+} else if (runnerAge > 18 && !registeredEarly){
+    console.log(`Late adults run at 11:00 am, your race number is ${raceNumber}`);
+}
+else if (runnerAge < 18){
+    console.log(`Youth registrants run at 12:30 pm. Your race number is ${raceNumber}. `)
+} else {
+    console.log('Please go to the registration desk');
 }
