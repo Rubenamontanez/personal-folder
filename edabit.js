6de46e2a455a53c67d9febe9283ef28ca692c9e4
Ruben Montanez <ruben.a.montanez@gmail.com>
2020-06-19 16:31:52 -0500

    more js

--- edabit.js
+++ edabit.js
@@ -31,3 +31,40 @@ const howOld = (age,year) => {
 }
 
 console.log(howOld(36,2040));
+
+const tipCalculator = (quality, total) => {
+    switch (quality) {
+        case 'bad':
+            return total * .05;
+        case 'ok':
+            return total * .15;
+        case 'good':
+            return total * .20;
+        case 'excellent':
+            return total * .30;
+        default:
+            return total * .18;
+
+    }
+}
+console.log(tipCalculator('good', 100))
+
+const toEmoticon = (emotion) =>{
+    switch(emotion){
+        case 'shrug':
+            return '|_{"}_|';
+        case 'smiley face':
+            return ':)';
+        case 'frowny face':
+            return ':(';
+        case 'winky face':
+            return ';)';
+        case 'heart':
+            return '<3';
+        default:
+            return '|_(* ~ *)_|'
+
+    }
+}
+
+console.log(toEmoticon("whatever"))
